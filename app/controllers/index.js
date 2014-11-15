8088a1dc2743ae97bae288c8057effac5d0da68f
mohamadhajj <mohd_r_hajj@yahoo.com>
2014-11-15 10:52:22 +0200

    test

---
+++ app/controllers/index.js
@@ -0,0 +1,19 @@
+function doClick(e) {
+    alert($.label.text);
+}
+
+function loginclick(e){
+	
+	if($.textbox1.value!=""&& $.textbox2.value!="") {
+		
+		
+  alert("ok ok ok ok ok");
+	}
+	
+	
+	else alert("faild");
+	
+	
+	}
+
+$.index.open();
